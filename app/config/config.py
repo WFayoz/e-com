8d174b9fad7fbcedb3e34a31e7fd3d8bd7edfd4b
wfayoz <wfayoz@yahoo.com>
2026-02-24 17:46:35 +0500

    structure...

---
+++ app/config/config.py
@@ -0,0 +1,35 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    POSTGRES_HOST: str = Field(default='localhost')
+    POSTGRES_PORT: int = Field(default=5432)
+    POSTGRES_DATABASE: str = Field(default='ecom')
+    POSTGRES_USER: str = Field(default='postgres')
+    POSTGRES_PASSWORD: str = Field(default='1')
+
+    # JWT
+    JWT_SECRET_KEY: str = Field(default='secret')
+    JWT_ALGORITHM: str = Field(default='HS256')
+    JWT_ACCESS_TOKEN_EXPIRE_TIME: int = Field(default=60)
+    JWT_REFRESH_TOKEN_EXPIRE_TIME: int = Field(default=3600)
+
+    # redis
+    REDIS_URL: str = Field(default='redis://localhost:6379/1')
+
+    @property
+    def postgres_sync_url(self):
+        return (f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+                f"{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}")
+
+    @property
+    def postgres_async_url(self):
+        return (f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+                f"{self.POSTGRES_PORT}/{self.POSTGRES_DATABASE}")
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
---
+++ app/models/users.py
@@ -0,0 +1,20 @@
+from pydantic import EmailStr
+from sqlalchemy import Integer, String, select
+from sqlalchemy.orm import Mapped, mapped_column
+
+from base_model import Model
+from base_model import db
+
+
+class User(Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    first_name: Mapped[str] = mapped_column(String(255), nullable=True)
+    phone_number: Mapped[str] = mapped_column(String(25), nullable=True, unique=True)
+    email: Mapped[EmailStr] = mapped_column(String(150), nullable=True, unique=True)
+    username: Mapped[str] = mapped_column(String(255), nullable=True, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=True)
+
+    @classmethod
+    async def get_by_username(cls, username: str):
+        query = select(cls).where(cls.username == username)
+        return (await db.execute(query)).scalar()
